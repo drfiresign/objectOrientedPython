c25548ed7e9fa6b370970477e737ac5b2da4dae8
Dylan Cascio <Dcascio73@gmail.com>
2016-09-13 10:43:04 -0700

    Initialized Object-Orientated Python repo
    Started new Course on Object-Orientated Python. This course will focus
    on creating a new version of the dungeon game from the last course, but
    will be more indepth and feature new functionality.
    
    Also created first code challenge.

---
+++ monster.py
@@ -0,0 +1,11 @@
+class Monster():
+    """Instance of Monster.
+
+
+    Attributes:
+    * hit_points, color, weapon.
+
+    Dislikes fire."""
+    hit_points = 1
+    color = 'yellow'
+    weapon = 'sword'
